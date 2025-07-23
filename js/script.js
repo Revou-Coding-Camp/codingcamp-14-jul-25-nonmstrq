e02376f4faf7954f10dc17eb1114c9d95db792e5
Revou-Coding-Camp <restuagilpurnomoadi60@gmail.com>
2025-07-24 06:06:37 +0700

    update form dan message output

--- js/script.js
+++ js/script.js
@@ -12,11 +12,28 @@ function showWelcomePopup() {
 
 function validateForm() {
     const nameInput = document.getElementById('name-input');
+    const dobInput = document.getElementById('dob-input');
+    const genderInput = document.getElementById('gender-input');
+    const messageInput = document.getElementById('message-input')
 
     if (nameInput.value === '') {
-        alert('Please enter your name.');
+        alert('Please fill in the blank fields.');
+    } else if(dobInput.value === '') {
+        alert('Please fill in the blank fields.');
+    } else if(genderInput.value === '') {
+        alert('Please fill in the blank fields.')
+    } else if(messageInput.value === '') {
+        alert('Please fill in the blank fields.')
     } else {
-        document.getElementById('message-output').innerHTML = `Thank you, ${nameInput.value}, for your message!`;
-        nameInput.value = ''; // Clear the input field after submission
+        document.getElementById('message-output').innerHTML = `<p class="mx-2 mt-2">Name: ${nameInput.value}</p>
+        <p class="mx-2 mt-2">Date of birth: ${dobInput.value}</p>
+        <p class="mx-2 mt-2">Gender: ${genderInput.value}</p>
+        <p class="mx-2 my-2">Message: ${messageInput.value}</p>`;
+        
+        // Clear the input field after submission
+        nameInput.value = '';
+        dobInput.value = '';
+        genderInput.value = '';
+        messageInput.value = '';
     }
 }
